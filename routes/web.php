4185d1d042cf7bda6d21388ca0db6a9d139c8f2c
RyoKojima <ryokojima@kojima.com>
2019-03-21 12:40:13 +0900

    タスク作成処理を追加

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,21 @@ Route::get('/', function () {
 });
 
 Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+
+    if($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 Route::delete('/task/{id}', function ($id) {
