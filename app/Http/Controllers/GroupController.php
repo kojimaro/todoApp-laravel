09b356c420e79fe11f29ade76d6a8f3f764363b5
RyoKojima <ryokojima@kojima.com>
2019-03-28 14:00:07 +0900

    group一覧を取得・表示する処理を追加

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\GroupRepository;
 
 class GroupController extends Controller
 {
     /**
+     * @var GroupRepository
+     */
+    protected $groups;
+
+    /**
      * @return void
      */
-    public function __construct()
+    public function __construct(GroupRepository $groups)
     {
         $this->middleware('auth');
+        $this->groups = $groups;
     }
 
     public function index(Request $request) {
-        return view('/groups.index');
+        $groups = $this->groups->forOwner($request->user());
+
+        return view('/groups.index', ['groups' => $groups]);
     }
 
     /**
--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -31,4 +31,46 @@
             </form>
         </div>
     </div>
+
+    <!-- TODO: グループ一覧 -->
+    @if (count($groups) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                グループ一覧
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- テーブルヘッダー -->
+                    <thead>
+                        <th>Group</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- テーブルボディー -->
+                    <tbody>
+                        @foreach ($groups as $group)
+                            <tr>
+                                <!-- タスク名 -->
+                                <td class="table-text">
+                                    <div>{{ $group->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: 削除ボタン -->
+                                    <form action="/group/{{ $group->id }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button>グループ削除</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
