e1f7c186716bc9436958af065a71960392679f93
RyoKojima <ryokojima@kojima.com>
2019-03-28 14:37:56 +0900

    Groupモデルのポリシーを追加

---
+++ app/Policies/GroupPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Group;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class GroupPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function onlyOwner(User $user, Group $group)
+    {
+        return $user->id === $group->user_id;
+    }
+}
