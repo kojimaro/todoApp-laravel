01dc8c4829b17aecba571b6f91e133d92787e042
RyoKojima <ryokojima@kojima.com>
2019-03-29 15:45:13 +0900

    グループ作成フォームへのリンクをメニューに追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <!-- グループ作成 -->
+                                    <a class="dropdown-item" href="{{ route('groups.index') }}">
+                                        {{ __('Group') }}
+                                    </a>
+
+                                    <!-- ログアウト -->
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
