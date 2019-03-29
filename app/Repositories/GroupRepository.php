2aa27c7dca127b6e25c540c7ef433ce8bd945e77
RyoKojima <ryokojima@kojima.com>
2019-03-29 14:45:07 +0900

    指定したIDでグループを取得する処理を追加

--- app/Repositories/GroupRepository.php
+++ app/Repositories/GroupRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\User;
 use App\Group;
+use PhpParser\Node\Stmt\GroupUse;
 
 class GroupRepository
 {
@@ -16,5 +17,17 @@ class GroupRepository
     public function forOwner(User $user)
     {
         return Group::where('user_id', $user->id)->orderBy('created_at', 'asc')->get();
+    }
+
+    /**
+     * 特定グループ取得
+     * @param string $groupId
+     * @return Model
+     * @throws ModelNotFoundException 404
+     */
+    public function getGroupId(string $groupId)
+    {
+        $groupId = (int) $groupId;
+        return Group::findOrFail($groupId);
     }
 }
