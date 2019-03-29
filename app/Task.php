bd793932fb54f44238902fde2d733741d6271853
RyoKojima <ryokojima@kojima.com>
2019-03-29 14:29:30 +0900

    group_idの複数代入を許可

--- app/Task.php
+++ app/Task.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'group_id'];
 
     public function user() {
         return $this->belongsTo(User::class);
